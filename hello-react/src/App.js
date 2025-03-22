87ff787239b68d95b94b4bb43f8b626183d7d214
SiWa1903 <sigitwaluyo960@gmail.com>
2025-03-22 13:02:52 +0700

    Pertemuan 2

---
+++ hello-react/src/App.js
@@ -0,0 +1,23 @@
+// import libaries
+import React from 'react';
+// create Component
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+const app = () => {
+  return (
+    <div>
+    <h1>Hello React</h1>
+    <p>My name is Hikasshi</p>
+    <Header/>
+    <Header/>
+    <Header/>
+    <Footer/>
+    <Footer/>
+    <Footer/>
+    </div>
+  );
+}
+
+// export component
+export default app;
---
+++ hello-react/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Footer() {
+      return (
+        <div>
+          <h3>Ini Halaman Footer</h3>
+          <footer>Desain by Hikasshi 😁👍</footer>
+        </div>
+      );
+    }
+export default Footer;
